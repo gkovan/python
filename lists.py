4fc33be87911b44e888c2fd0aa38a3ad44958f19
gkovan <gkovan@hotmail.com>
2020-07-15 09:34:13 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,14 @@
+print("Learning python lists")
+
+fruitList = ["apple", "banana", "cherry"]
+print(fruitList)
+
+for fruit in fruitList:
+    print(fruit)
+
+listOfNums = [1, 2, 3, 4, 5]
+total = 0
+for num in listOfNums:
+    total = total + num
+
+print(total)
